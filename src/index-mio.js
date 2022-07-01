8c375943b8c59e8a024c43eb27286fedc4988890
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-06-30 21:44:26 -0300

    agregue componente Tarjeta y renderizarlo en index

---
+++ src/index-mio.js
@@ -0,0 +1,11 @@
+import { Tarjeta } from './tarea/Tarea1.js';
+
+ReactDOM.render(
+  <Tarjeta
+    nombre="Julio Gagliardi"
+    titulo="Programador Front End"
+    imagen="https://avatars.githubusercontent.com/u/93630700?s=400&u=15b5e059d8912043babef0c3ca4cc9081c7ce37b&v=4"
+  />,
+  document.getElementById('react-app')
+);
+// ReactDOM.render(<Tarjeta />, document.getElementById('react-app'));
